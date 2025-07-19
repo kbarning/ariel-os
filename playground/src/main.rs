a2e1d1b75874ba402ed8635d6441170c3beefaba
Kilian Barning <barning@uni-bremen.de>
2025-07-19 22:34:17 +0200

    Add mpu to context switch

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,11 +2,14 @@
 #![no_std]
 #![allow(unconditional_panic)]
 
-use ariel_os::debug::log::*;
+use ariel_os::debug::{log::*, println};
 use ariel_os::thread::*;
+use cortex_m::register::msp;
 
 #[ariel_os::thread(autostart)]
 fn thread_a() {
+    let stack_begin = msp::read();
+    println!("Stack begin of tread a at: {:x}", stack_begin);
     info!("Thread A Running");
 
     for _ in 0..10 {
--- src/ariel-os-rt/src/mpu.rs
+++
@@ -1,5 +0,0 @@
-use ariel_os_mpu;
-
-pub fn enable() {
-    ariel_os_mpu::enable_mpu();
-}
