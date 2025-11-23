7b07679e0275ef8fef442f0fcb28e17a0eeb6439
Kilian Barning <barning@uni-bremen.de>
2025-11-23 03:09:21 +0100

    Small improvements

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -7,16 +7,15 @@ use core::mem::MaybeUninit;
 use ariel_os::debug::{ExitCode, exit, log::*};
 use ariel_os::thread::*;
 
+#[allow(unconditional_recursion)]
+fn recursion() {
+    let arr: MaybeUninit<[u8; 1]> = MaybeUninit::uninit();
+    core::hint::black_box(arr);
+    recursion();
+}
+
 #[ariel_os::thread(autostart)]
 fn thread_a() {
-    // 20003030 -> 20003050
-
-    unsafe {
-        core::ptr::write(0x2000305f as *mut u8, 0x12);
-    }
-
-    recursion(0);
-
     for _ in 0..1000 {
         info!("Thread A Looping 1");
     }
@@ -30,18 +29,6 @@ fn thread_a() {
     yield_same();
 }
 
-#[allow(unconditional_recursion)]
-fn recursion(i: usize) {
-    let arr: MaybeUninit<[u8; 1]> = MaybeUninit::uninit();
-    core::hint::black_box(arr);
-    // Use to halt in debugger mode one step bevor overflow
-    // let sp = cortex_m::register::psp::read();
-    // if sp < 0x20003090 {
-    //     cortex_m::asm::bkpt();
-    // }
-    recursion(i + 1);
-}
-
 #[ariel_os::thread(autostart)]
 fn thread_b() {
     info!(
@@ -49,6 +36,12 @@ fn thread_b() {
         cortex_m::register::pc::read(),
         cortex_m::register::psp::read()
     );
+
+    // unsafe {
+    //     core::ptr::write(0x2000387f as *mut u8, 0x12);
+    // }
+
+    recursion();
 
     for _ in 0..100 {
         info!("Thread B Looping 1");
@@ -58,31 +51,6 @@ fn thread_b() {
 
     for _ in 0..10 {
         info!("Thread B Looping 2");
-    }
-
-    yield_same();
-}
-
-#[ariel_os::thread(autostart)]
-fn thread_c() {
-    info!(
-        "Thread C Running at address {:x} and sp {:x}",
-        cortex_m::register::pc::read(),
-        cortex_m::register::psp::read()
-    );
-
-    for _ in 0..10 {
-        info!("Thread C Looping 1");
-    }
-
-    for _ in 0..10 {
-        info!("Thread C Looping 2");
-    }
-
-    yield_same();
-
-    for _ in 0..10 {
-        info!("Thread C Looping 3");
     }
 
     exit(ExitCode::SUCCESS);
