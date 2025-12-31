aafa54047cc96f448884e26b1bc18063923b8f36
Kilian Barning <barning@uni-bremen.de>
2025-12-31 16:07:50 +0100

    WIP

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,6 +2,8 @@
 #![no_std]
 #![allow(unconditional_panic)]
 
+use core::mem::MaybeUninit;
+
 use ariel_os::{
     debug::{ExitCode, exit, log::info},
     thread::current_stack_limits,
@@ -9,17 +11,26 @@ use ariel_os::{
 
 #[allow(unconditional_recursion)]
 fn recursion() {
-    // Ein Byte auf dem Stapelspeicher allokieren
-    let arr: [u8; 1] = [0xff];
-    // Verhindert, das diese Variable von dem Übersetzter wegoptimiert wird
+    // Ein Byte auf dem Stapelspeicher allokieren. Dies verhindert, dass der Übersetzter
+    // die Rekursion wegoptimiert (Tail-Call-Optimization)
+    let mut arr = [0u32; 10];
+    // Verhindert, dass die Variable wegoptimiert wird
     core::hint::black_box(arr);
+    // Zugriff auf ein Element in dem Array, welches hinter der 4 Bytes Grenze liegt:
+    arr[6] = 10;
+    core::hint::black_box(arr[6]);
     // Rekursiver Aufruf
     recursion();
 }
 
 #[ariel_os::thread(autostart)]
-fn main() {
-    info!("Stack start: {:x}", current_stack_limits().unwrap().0,);
+fn a() {
+    info!(
+        "Thread a started, stack limit: 0x{:x}",
+        current_stack_limits().unwrap().0
+    );
+    // Rekursion starten
     recursion();
+    // Wird nie erreicht werden
     exit(ExitCode::SUCCESS);
 }
