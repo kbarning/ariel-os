e8d4e5bd748f12032dfc76a1330ef6fb78b60b92
Kilian Barning <barning@uni-bremen.de>
2026-01-03 23:12:52 +0100

    Scheduler context switch benchmark. MPU is enabled.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -2,32 +2,9 @@
 #![no_std]
 #![allow(unconditional_panic)]
 
-use ariel_os::{
-    debug::{ExitCode, exit, log::info},
-    thread::current_stack_limits,
-};
-
-fn recursion() {
-    // Ein 10 Bytes auf dem Stapelspeicher allokieren. Dies verhindert, dass der Übersetzter
-    // die Rekursion wegoptimiert (Tail-Call-Optimization)
-    let mut arr = [0u32; 10];
-    // Verhindert, dass die Variable wegoptimiert wird
-    core::hint::black_box(arr);
-    // Zugriff auf ein Element in dem Array, welches hinter der 4 Bytes Grenze liegt:
-    arr[6] = 10;
-    core::hint::black_box(arr[6]);
-    // Rekursiver Aufruf
-    recursion();
-}
+use ariel_os::debug::{ExitCode, exit};
 
 #[ariel_os::thread(autostart)]
 fn a() {
-    info!(
-        "Thread a started, stack limit: 0x{:x}",
-        current_stack_limits().unwrap().0
-    );
-    // Rekursion starten
-    recursion();
-    // Wird nie erreicht werden
     exit(ExitCode::SUCCESS);
 }
