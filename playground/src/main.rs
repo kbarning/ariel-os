83380a54e70869e1a3555049a92462bc1f7682ab
Kilian Barning <barning@uni-bremen.de>
2026-01-03 22:04:21 +0100

    Sandboxing Test that tries executing binary instructions from the stack. MPU is disabled

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,27 +3,19 @@
 #![allow(unconditional_panic)]
 
 use ariel_os::debug::{ExitCode, exit, log::info};
-use ariel_os::{mpu, thread::*};
+use ariel_os::thread::*;
 
 #[ariel_os::thread(autostart)]
 fn sandbox() {
     info!("Starting sandbox thread");
     // Hier wird der C-Binärcode statisch in dem Flash-Speicher mithilfe des Übersetzers platziert
     const C_BINARY: &[u8; 32] = include_bytes!("../../c_program/main.bin");
-    let (stack_start, stack_end) = current_stack_limits().unwrap();
-    // Stapelspeicher als les- und beschreibbar konfigurieren
-    mpu::configure_stack(stack_start..=stack_end);
-    // MPU einschalten
-    mpu::enable();
     unsafe {
         // Umwandeln des Array in ein Funktionszeiger
         let c_entry: extern "C" fn() = core::mem::transmute(C_BINARY.as_ptr());
         // Aufruf dieses Funktionszeigers
         c_entry();
     }
-    // MPU wieder ausschalten, damit Ariel-OS wieder auf globale Variablen zugreifen kann
-    // Diese werden benötigt,
-    mpu::disable();
     info!("C-Binary returned successfully");
     exit(ExitCode::SUCCESS);
 }
