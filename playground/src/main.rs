6bf947eeb556726ae99e3433f4325d841a240f2b
Kilian Barning <barning@uni-bremen.de>
2026-01-03 22:15:54 +0100

    Stack Redzone Test that tries to stack overflow on another thread, but the array is too large. MPU is enabled

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -7,13 +7,15 @@ use ariel_os::{
     thread::current_stack_limits,
 };
 
-#[allow(unconditional_recursion)]
 fn recursion() {
-    // Ein Byte auf dem Stapelspeicher allokieren. Dies verhindert, dass der Übersetzter
-    // Die Rekursion wegoptimiert (Tail-Call-Optimization)
-    let arr: [u8; 1] = [0xff];
+    // Ein 10 Bytes auf dem Stapelspeicher allokieren. Dies verhindert, dass der Übersetzter
+    // die Rekursion wegoptimiert (Tail-Call-Optimization)
+    let mut arr = [0u32; 10];
     // Verhindert, dass die Variable wegoptimiert wird
     core::hint::black_box(arr);
+    // Zugriff auf ein Element in dem Array, welches hinter der 4 Bytes Grenze liegt:
+    arr[6] = 10;
+    core::hint::black_box(arr[6]);
     // Rekursiver Aufruf
     recursion();
 }
@@ -22,18 +24,6 @@ fn recursion() {
 fn a() {
     info!(
         "Thread a started, stack limit: 0x{:x}",
-        current_stack_limits().unwrap().0
-    );
-    // Wechselt zu Thread b
-    ariel_os::thread::yield_same();
-    // Wird nie erreicht werden
-    exit(ExitCode::SUCCESS);
-}
-
-#[ariel_os::thread(autostart)]
-fn b() {
-    info!(
-        "Thread b started, stack limit: 0x{:x}",
         current_stack_limits().unwrap().0
     );
     // Rekursion starten
